9887a45badd3831c407efd1697ed67b68f66e61b
Nathan Curtis <nathancurtis951@gmail.com>
2026-03-29 23:09:36 -0700

    cleanup: remove unused files, old screenshots, debug plugin, dead components

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { logPlugin } from './vite-log-plugin.js'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), logPlugin()],
+  plugins: [react(), tailwindcss()],
   base: '/',
   resolve: {
     alias: {
